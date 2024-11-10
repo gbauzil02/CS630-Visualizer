0bb4c58438ea5c3d8ac5a82238ae7cfe5eeefb4a
kadeem-lewis <email@kadeemlewis.com>
2024-11-10 01:48:21 -0500

    updated task manager to display added processes

--- client/src/components/TaskManager.tsx
+++ client/src/components/TaskManager.tsx
@@ -1,22 +1,21 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import ProcessCard from "@/components/ProcessCard";
+import { useProcessContext } from "@/contexts/ProcessContext";
 
 export default function TaskManager() {
+  const { processes } = useProcessContext();
+
   return (
     <Card className="grow-1">
       <CardHeader>
         <CardTitle>Task Manager</CardTitle>
       </CardHeader>
       <CardContent>
-        <ProcessCard
-          process={{
-            pid: "1",
-            hasIO: true,
-            IOStatus: "Waiting",
-            size: 10,
-            state: "Ready",
-          }}
-        />
+        <div className="flex gap-2 flex-col overflow-y-auto">
+          {processes.map((process) => (
+            <ProcessCard key={process.pid} process={process} />
+          ))}
+        </div>
       </CardContent>
     </Card>
   );
