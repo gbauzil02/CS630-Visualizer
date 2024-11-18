4a3635ed9b483b388817f8f6cf8e0a509d3dceb5
kadeem-lewis <email@kadeemlewis.com>
2024-11-18 06:52:44 -0500

    replaced pixijs code in app with new container component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,48 @@
 import { Button } from "@/components/ui/button";
-import { Container, Stage, Text } from "@pixi/react";
-import { Circle } from "@/components/Circle";
-import { TextStyle } from "pixi.js";
 import SettingsPanel from "@/components/SettingsPanel";
 import TaskManager from "@/components/TaskManager";
 import { Toaster } from "@/components/ui/toaster";
 import { ProcessContextProvider } from "@/contexts/ProcessContext";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { socket } from "@/services/socket.ts";
+import Container from "./components/Container";
 
 function App() {
-  const [isRunning, setIsRunning] = useState(false)
-
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-      socket.on('connect',() => {
-        console.log("Connected to server")
-      });
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
 
-      socket.on("processes",(data)=>{
-        console.log(data)
-      })
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
 
-      socket.on("test",(data)=>{
-        console.log(data)
-      })
+    socket.on("test", (data) => {
+      console.log(data);
+    });
 
-      return () => {
-        socket.off('connect');
-        socket.off("processes")
-      }; 
-  },[])
+    socket.on("processes", (data) => {
+      console.log(data);
+    });
 
-  async function startSimulation(){
+    return () => {
+      socket.off("connect");
+      socket.disconnect();
+    };
+  }, []);
+
+  async function startSimulation() {
     socket.connect();
     socket.emit("start");
-    setIsRunning(true)
+    setIsRunning(true);
   }
 
-  async function stopSimulation(){
-    setIsRunning(false)
+  async function stopSimulation() {
+    setIsRunning(false);
   }
 
-  
   return (
     <>
       <main className="mx-auto max-w-7xl space-y-4">
@@ -52,29 +52,18 @@ function App() {
         <ProcessContextProvider>
           <div className="flex gap-4">
             <div className="w-full flex flex-col gap-4">
-              <Stage>
-                <Container>
-                  <Text
-                    text="Hello World!"
-                    x={150}
-                    y={150}
-                    style={
-                      new TextStyle({
-                        fill: "white",
-                        fontSize: 36,
-                        fontFamily: "Arial",
-                      })
-                    }
-                  />
-                  <Circle />
-                </Container>
-              </Stage>
+              <Container />
               {isRunning ? (
-                <Button className=" self-end" onClick={stopSimulation}> Stop </Button>
-              ):(
-                <Button className=" self-end" onClick={startSimulation}>Run</Button>
+                <Button className=" self-end" onClick={stopSimulation}>
+                  {" "}
+                  Stop{" "}
+                </Button>
+              ) : (
+                <Button className=" self-end" onClick={startSimulation}>
+                  Run
+                </Button>
               )}
-              
+
               <SettingsPanel />
             </div>
             <TaskManager />
