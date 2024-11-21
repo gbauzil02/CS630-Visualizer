d10ae1a3ef9c0b69bf215346fca0a62f3b4634df
kadeem-lewis <email@kadeemlewis.com>
2024-11-20 22:16:47 -0500

    updated state title to be bolder and processes to be a circle

--- client/src/components/State.tsx
+++ client/src/components/State.tsx
@@ -1,5 +1,5 @@
 import { motion } from "motion/react";
-import type { Process } from "./SettingsPanel";
+import type { Process } from "@/components/SettingsPanel";
 
 type StateProps = {
   name: string;
@@ -10,15 +10,15 @@ type StateProps = {
 export default function State({ name, className, processes }: StateProps) {
   return (
     <div className={className}>
-      <h2>{name}</h2>
-      <motion.ul className="rounded-lg h-16 border border-black bg-muted px-1 flex gap-1 overflow-x-auto">
+      <h2 className="font-bold">{name}</h2>
+      <motion.ul className="h-16 border border-black bg-muted px-1 flex gap-1 overflow-x-auto">
         {processes
           .filter((process) => process.state === name)
           .map((process) => (
             <motion.li
               key={process.pid}
               layoutId={`process-${process.pid}`}
-              className="p-2 self-center font-bold bg-green-500 text-white rounded-full shadow"
+              className="p-2 self-center font-bold bg-green-500 text-white rounded-full shadow flex items-center justify-center w-10 h-10"
             >
               {process.pid}
             </motion.li>
